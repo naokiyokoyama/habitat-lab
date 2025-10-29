6b3afc28342ef047d4804a558b99a646f34df9a3
Naoki Yokoyama <nyokoyama@apple.com>
2025-10-29 15:58:40 -0700

    allowing scenes to be mutated

--- habitat-baselines/habitat_baselines/common/construct_vector_env.py
+++ habitat-baselines/habitat_baselines/common/construct_vector_env.py
@@ -47,6 +47,8 @@ def construct_envs(
             "No scenes to load, multiple process logic relies on being able to split scenes uniquely between processes"
         )
 
+    # Convert to mutable list to avoid ReadonlyConfigError when shuffling
+    scenes = list(scenes)
     random.shuffle(scenes)
 
     scene_splits: List[List[str]] = [[] for _ in range(num_environments)]
